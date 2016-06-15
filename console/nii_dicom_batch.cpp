de1070b0e1e0684f7d6c1aa97f413f124224b35c
Chris Gorgolewski <krzysztof.gorgolewski@gmail.com>
2016-06-15 11:53:26 -0400

    added Manufacturer and field strength fields to BIDS output

--- console/nii_dicom_batch.cpp
+++ console/nii_dicom_batch.cpp
@@ -354,7 +354,22 @@ void nii_SaveBIDS(char pathoutname[], struct TDICOMdata d, struct TDCMopts opts,
     //printf("Saving DTI %s\n",txtname);
     FILE *fp = fopen(txtname, "w");
     fprintf(fp, "{\n");
-	fprintf(fp, "\t\"EchoTime\": %g,\n", d.TE / 1000.0 );
+		switch (d.manufacturer) {
+				case kMANUFACTURER_SIEMENS:
+						fprintf(fp, "\t\"Manufacturer\": \"Simens\",\n" );
+						break;
+				case kMANUFACTURER_GE:
+						fprintf(fp, "\t\"Manufacturer\": \"GE\",\n" );
+						break;
+				case kMANUFACTURER_PHILIPS:
+						fprintf(fp, "\t\"Manufacturer\": \"Phillips\",\n" );
+						break;
+				case kMANUFACTURER_TOSHIBA:
+						fprintf(fp, "\t\"Manufacturer\": \"Toshiba\",\n" );
+						break;
+		};
+		fprintf(fp, "\t\"MagneticFieldStrength\": %g,\n", d.fieldStrength );
+	  fprintf(fp, "\t\"EchoTime\": %g,\n", d.TE / 1000.0 );
     fprintf(fp, "\t\"RepetitionTime\": %g,\n", d.TR / 1000.0 );
     if ((d.CSA.bandwidthPerPixelPhaseEncode > 0.0) &&  (h->dim[2] > 0) && (h->dim[1] > 0)) {
 		float dwellTime = 0;
