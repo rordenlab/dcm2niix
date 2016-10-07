e713244dc6b39552a9c8a08971672ca61f1bdedb
Chris Rorden <rorden@mailbox.sc.edu>
2016-10-07 16:31:22 -0400

    restore mingw support

--- console/nii_dicom_batch.cpp
+++ console/nii_dicom_batch.cpp
@@ -327,6 +327,34 @@ void nii_SaveText(char pathoutname[], struct TDICOMdata d, struct TDCMopts opts,
     fclose(fp);
 }
 
+#ifdef __MINGW32__
+/* http://stackoverflow.com/questions/12975022/strtok-r-for-mingw
+ * public domain strtok_r() by Charlie Gordon
+ *
+ *   from comp.lang.c  9/14/2007
+ *
+ *      http://groups.google.com/group/comp.lang.c/msg/2ab1ecbb86646684
+ *
+ *     (Declaration that it's public domain):
+ *      http://groups.google.com/group/comp.lang.c/msg/7c7b39328fefab9c
+ */
+
+char* strtok_r( char *str, const char *delim, char **nextp) {
+    char *ret;
+    if (str == NULL)
+        str = *nextp;
+    str += strspn(str, delim);
+    if (*str == '\0')
+        return NULL;
+    ret = str;
+    str += strcspn(str, delim);
+    if (*str)
+        *str++ = '\0';
+    *nextp = str;
+    return ret;
+}
+#endif
+
 void nii_SaveBIDS(char pathoutname[], struct TDICOMdata d, struct TDCMopts opts, struct TDTI4D *dti4D, struct nifti_1_header *h) {
 //https://docs.google.com/document/d/1HFUkAEE-pB-angVcYe6pf_-fVf4sCpOHKesUvfb8Grc/edit#
 // Generate Brain Imaging Data Structure (BIDS) info
