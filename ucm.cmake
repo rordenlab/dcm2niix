2fe167e1d48588c2aabc7cc5bb92d97d3fb4723a
Ningfei Li <ningfei.li@gmail.com>
2016-10-29 23:14:13 +0200

    Update CMakeLists.txt 1. Add options to choose runtime and build type 2. Fix compatibility issue with regarding to CMP0054 in CMake>=3.1
    Signed-off-by: Ningfei Li <ningfei.li@gmail.com>

--- ucm.cmake
+++ ucm.cmake
@@ -25,8 +25,8 @@ else()
     set(ucm_with_cotire 0)
 endif()
 
-option(UCM_UNITY_BUILD          "Enable unity build for targets registered with the ucm_add_target() macro"                     OFF)
-option(UCM_NO_COTIRE_FOLDER     "Do not use a cotire folder in the solution explorer for all unity and cotire related targets"  ON)
+# option(UCM_UNITY_BUILD          "Enable unity build for targets registered with the ucm_add_target() macro"                     OFF)
+# option(UCM_NO_COTIRE_FOLDER     "Do not use a cotire folder in the solution explorer for all unity and cotire related targets"  ON)
 
 # ucm_add_flags
 # Adds compiler flags to CMAKE_<LANG>_FLAGS or to a specific config
