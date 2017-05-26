1a1e785ed4d1a5bb1ab36034256323497ce84b1d
Chris Rorden <rorden@mailbox.sc.edu>
2017-05-26 14:37:08 -0400

    Attempt to restore Windows compatibility

--- console/nii_dicom_batch.cpp
+++ console/nii_dicom_batch.cpp
@@ -361,31 +361,24 @@ bool isDerived(struct TDICOMdata d) {
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  */
-void * memmem(const void *l, size_t l_len, const void *s, size_t s_len)
-{
+void * memmem(const char *l, size_t l_len, const char *s, size_t s_len) {
 	register char *cur, *last;
 	const char *cl = (const char *)l;
 	const char *cs = (const char *)s;
-
 	/* we need something to compare */
 	if (l_len == 0 || s_len == 0)
 		return NULL;
-
 	/* "s" must be smaller or equal to "l" */
 	if (l_len < s_len)
 		return NULL;
-
 	/* special case where s_len == 1 */
 	if (s_len == 1)
 		return memchr(l, (int)*cs, l_len);
-
 	/* the last position where its possible to find "s" in "l" */
 	last = (char *)cl + l_len - s_len;
-
 	for (cur = (char *)cl; cur <= last; cur++)
 		if (cur[0] == cs[0] && memcmp(cur, cs, s_len) == 0)
 			return cur;
-
 	return NULL;
 }
 #endif //for systems without memmem
