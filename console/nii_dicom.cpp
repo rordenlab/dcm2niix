dd8fc65f39fe25304735c6250f76fef1cd02f74e
Jon Clayden <code@clayden.org>
2020-08-25 16:54:13 +0100

    Bounding string concatenation to avoid compiler warnings

--- console/nii_dicom.cpp
+++ console/nii_dicom.cpp
@@ -6951,9 +6951,8 @@ if (d.isHasPhase)
 	}
 	if ((isDICOMANON) && (isMATLAB)) {
 		//issue 383
-		d.seriesInstanceUID[0] = '\0';
-		strcat(d.seriesInstanceUID, d.studyDate);
-		strcat(d.seriesInstanceUID, d.studyTime);
+		strcpy(d.seriesInstanceUID, d.studyDate);
+		strncat(d.seriesInstanceUID, d.studyTime, kDICOMStr-strlen(d.studyDate));
 		d.seriesUidCrc = mz_crc32X((unsigned char*) &d.seriesInstanceUID, strlen(d.seriesInstanceUID));
 	}
     if ((d.manufacturer == kMANUFACTURER_SIEMENS) && (strstr(d.sequenceName, "_fl2d1") != NULL)) {
