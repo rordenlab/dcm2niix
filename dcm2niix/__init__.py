3f698b9c86428b600e715602b623b0dcfa873f47
Casper da Costa-Luis <casper.dcl@physics.org>
2022-01-15 12:25:35 +0000

    add executable

--- dcm2niix/__init__.py
+++ dcm2niix/__init__.py
@@ -11,9 +11,17 @@ except ImportError: # pragma: nocover
         __version__ = get_version(root="../..", relative_to=__file__)
     except (ImportError, LookupError):
         __version__ = "UNKNOWN"
-__all__ = ['bin', 'bin_path']
+__all__ = ['bin', 'bin_path', 'main']
 
 from pathlib import Path
 
 bin_path = Path(__file__).resolve().parent / "dcm2niix"
 bin = str(bin_path)
+
+
+def main(args=None):
+    if args is None:
+        import sys
+        args = sys.argv[1:]
+    from subprocess import run
+    run([bin] + args)
