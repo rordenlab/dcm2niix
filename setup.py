26723410c977dd501cf57a9f244f9eccbcd86815
Casper da Costa-Luis <casper.dcl@physics.org>
2022-06-30 09:19:51 +0100

    remove unneeded DCM2NIIX_BUILD_VERSION

--- setup.py
+++ setup.py
@@ -10,5 +10,4 @@ build_ver = ".".join(__version__.split(".")[:3]).split(".dev")[0]
 for i in (Path(__file__).resolve().parent / "_skbuild").rglob("CMakeCache.txt"):
     i.write_text(re.sub("^//.*$\n^[^#].*pip-build-env.*$", "", i.read_text(), flags=re.M))
 setup(use_scm_version=True, packages=["dcm2niix"],
-      cmake_languages=("CXX",), cmake_minimum_required_version="3.18",
-      cmake_args=[f"-DDCM2NIIX_BUILD_VERSION={build_ver}"])
+      cmake_languages=("CXX",), cmake_minimum_required_version="3.18")
